00540a47ddf39144aeb94f41222b1244e587ac60
Ranvir Singh <Warherolion@users.noreply.github.com>
2025-07-08 20:24:18 -0400

    Created matlab script, created roll, pitch, yaw matrix

---
+++ Rocket_script_2023.m
@@ -0,0 +1,4 @@
+roll = readmatrix("2023_RocketData\GyroRollRate_TeleMega_2024.txt");
+pitch = readmatrix("2023_RocketData\GyroPitchRate_TeleMega_2024.txt");
+yaw = readmatrix("2023_RocketData\GyroYawRate_TeleMega_2024.txt");
+
