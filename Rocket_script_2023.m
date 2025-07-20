c5b1689ae47d3b1f6522ab48d35ebf75306bf894
Ranvir Singh <Warherolion@users.noreply.github.com>
2025-07-20 15:32:33 -0400

    Minor change to file for matrices

--- Rocket_script_2023.m
+++ Rocket_script_2023.m
@@ -1,8 +1,10 @@
-roll = readmatrix("2023_RocketData\GyroRollRate_TeleMega_2024.txt");
-pitch = readmatrix("2023_RocketData\GyroPitchRate_TeleMega_2024.txt");
-yaw = readmatrix("2023_RocketData\GyroYawRate_TeleMega_2024.txt");
+roll = readmatrix("2023_RocketData/GyroRollRate_TeleMega_2024.txt");
+pitch = readmatrix("2023_RocketData/GyroPitchRate_TeleMega_2024.txt");
+yaw = readmatrix("2023_RocketData/GyroYawRate_TeleMega_2024.txt");
 
-thrust = readmatrix("2023_RocketData\ThrustCurve.txt");
+thrust = readmatrix("2023_RocketData/ThrustCurve.txt");
+
+
 
 %Rocket body dimensions 
 
